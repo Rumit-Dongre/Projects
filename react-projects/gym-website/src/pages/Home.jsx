34763ae72490640105f7f8f531ac9795ef6c9f9f
Rumit <appidongre@gmail.com>
2025-02-09 02:08:23 +0530

    motion animation is bestfriedn now

--- react-projects/gym-website/src/pages/Home.jsx
+++ react-projects/gym-website/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router'
 import Navbar from '../Component/Navbar'
 
 import mainImage from '../images/gym.jpg'
+import { motion } from "motion/react"
 
 import '../styles/home.css'
 import Footer from '../Component/Footer'
@@ -17,22 +18,38 @@ const Home = () => {
 
         <div className="contain">
 
-          <h1>redefining the
+          <motion.h1
+            initial={{
+              y: -200,
+              color: "red"
+            }}
+            animate={{
+              y: 0,
+              color: "white"
+            }}
+            transition={{
+              duration: 2
+            }}
+          >redefining the
             <br />
             <span>Fitness</span> Experience
-          </h1>
+          </motion.h1>
 
 
           <h4>Helping you achieve your fitness goals</h4>
 
-          <button className='btn'>Book a free Sesssion</button>
+          <motion.button 
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+          drag
+          className='btn'>Book a free Sesssion</motion.button>
 
         </div>
 
 
       </div>
 
-      <Footer/>
+      <Footer />
 
     </>
   )
